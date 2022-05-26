bfa82b58d1642d521dfaa5e4f2d82f30f2e01535
Rick Debbout <debboutr@gmail.com>
2022-05-25 23:50:52 -0700

    dot miniconda

--- scripts/setup_pop.sh
+++ scripts/setup_pop.sh
@@ -109,11 +109,11 @@ else
 fi
 
 install_miniconda () {
-	mkdir -p ~/miniconda3
-	wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
-	bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
-	rm -rf ~/miniconda3/miniconda.sh
-	~/miniconda3/bin/conda init bash
+	mkdir -p ~/.miniconda3
+	wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/.miniconda3/miniconda.sh
+	bash ~/.miniconda3/miniconda.sh -b -u -p ~/.miniconda3
+	rm -rf ~/.miniconda3/miniconda.sh
+	~/.miniconda3/bin/conda init bash
 }
 
 if ! [ -x "$(command -v conda)" ]; then
