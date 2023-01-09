a66eb68f5c1031654cd42dc015ba41330f9ee8bf
debboutr <debboutr@gmail.com>
2023-01-09 08:29:53 -0800

    add vim-commentary

--- .config/nvim/lua/wreckedabout/packer.lua
+++ .config/nvim/lua/wreckedabout/packer.lua
@@ -30,10 +30,12 @@ return require('packer').startup(function(use)
   }
 
   use({'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'})
-  --use('nvim-treesitter/playground')
+  -- use('nvim-treesitter/playground')
+
   use('theprimeagen/harpoon')
   use('mbbill/undotree')
   use('tpope/vim-fugitive')
+  use('tpope/vim-commentary')
   use("vimwiki/vimwiki")
 
   use("mhinz/vim-signify")
