562d6a3e1dbd0bb83342974695bf257441ea640d
debboutr <debboutr@gmail.com>
2023-01-08 21:04:03 -0800

    splitbelow, splitright

--- .config/nvim/lua/wreckedabout/set.lua
+++ .config/nvim/lua/wreckedabout/set.lua
@@ -3,6 +3,9 @@ vim.opt.guicursor = ""
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
