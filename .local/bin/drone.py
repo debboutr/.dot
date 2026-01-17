1dde299b182c5340ccef82b92d3b1661c983b7f1
debboutr <debboutr@gmail.com>
2026-01-17 11:11:55 -0800

    no clean.sh when no action -- drone

--- .local/bin/drone.py
+++ .local/bin/drone.py
@@ -8,20 +8,22 @@ import os
 import sys
 
 fn = "clean.sh"
-text = 'ffmpeg -i {} -c:v h264_nvenc -rc:v vbr -b:v 10M -vf "scale=1920:1080" -r 30 -c:a aac -b:a 128k -movflags +faststart {}\n'
+text = ('ffmpeg -i {} -c:v h264_nvenc -rc:v vbr -b:v 10M'
+        ' -vf "scale=1920:1080" -r 30 -c:a aac -b:a 128k'
+        ' -movflags +faststart {}\n')
 print(os.getcwd())
 # print([f for f in os.listdir() if f.endswith('.MP4')])
 vids = [f for f in os.listdir() if f.startswith('DJI_') and f.endswith('.MP4')]
 if vids:
     os.mkdir("orig")
-with open(fn, "w") as f:
-    f.write('#!/bin/sh\n')
-    for v in vids:
-        n = v.replace("DJI", "CLN")
-        print(text.format(v, n))
-        f.write(text.format(v, n))
-        f.write(f"mv {v} orig\n")
+    with open(fn, "w") as f:
+        f.write('#!/bin/sh\n')
+        for v in vids:
+            n = v.replace("DJI", "CLN")
+            print(text.format(v, n))
+            f.write(text.format(v, n))
+            f.write(f"mv {v} orig\n")
 
-mode = os.stat(fn).st_mode
-mode |= 0o111
-os.chmod(fn, mode) 
+    mode = os.stat(fn).st_mode
+    mode |= 0o111
+    os.chmod(fn, mode) 
